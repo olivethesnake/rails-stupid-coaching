dab876683ea52ca343fe0b9d41fc674bc6742709
olivia franklin <frolivia@yahoo.fr>
2023-02-20 16:28:33 +0100

    bgig

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:answer]
+    if @question == "I am going to work"
+      @answer = 'Great!'
+    elsif @question == ''.end_with?('?')
+      @answer = 'question , get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
